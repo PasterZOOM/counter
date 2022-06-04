8c08ef7cf3a9c8edd36e2d9bc03ab35e59dea9de
Ilya Pasternak <pasterzoom@gmail.com>
2022-06-04 20:59:23 +0300

    React.StrictMode

--- src/components/input/InputNumber.tsx
+++ src/components/input/InputNumber.tsx
@@ -9,12 +9,11 @@ type InputNumberPropsType = {
 
 export const InputNumber: React.FC<InputNumberPropsType> = ({callBack, error, value}) => {
 
-    const onBlurHandler = () => isNaN(value) && callBack(0)
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => callBack(isNaN(value) ? 0 : e.currentTarget.valueAsNumber)
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) =>
+        callBack(isNaN(e.currentTarget.valueAsNumber) ? 0 : e.currentTarget.valueAsNumber)
 
     return <div>
         <input className={error ? style.error : style.input}
-               onBlur={onBlurHandler}
                onChange={onChangeHandler}
                type="number"
                value={value.toFixed()}
